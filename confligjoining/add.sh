b025ef242e6be9781ec692f46587c5e7d69e23bc
praveenkumarpgiindia <praveenkumarpgiindia@gmail.com>
2021-09-10 13:58:24 +0530

    add.sh finalised

--- confligjoining/add.sh
+++ confligjoining/add.sh
@@ -7,18 +7,20 @@ echo -e "The file to be joined are $x and $y"
 grep "LIG" $y > z.txt
 head $x -n -1 > p.txt
 tail $x -n 1 > t.txt
-cat p.txt z.txt t.txt ss
+cat p.txt z.txt t.txt > protein.processed.gro
 value1=`cat $x | wc -l` 
 deletingvalue1=3
 value1final=$((value1-deletingvalue1))
 value2=`grep "LIG" $y | wc -l` 
 valuefinal=$((value1final+value2))
 echo "$valuefinal will be added inplace to the output file"
+sed -i "2 i\ $valuefinal" protein.processed.gro 
+sed -i '3d' protein.processed.gro
 rm p.txt z.txt t.txt
 }
 echo "enter conf.gro"
 read first
 echo "enter lig.gro"
 read second
-add $first $sec 
+add $first $second 
 echo "end of the script"
