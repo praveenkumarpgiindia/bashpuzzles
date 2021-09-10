0374932c7e597064f3055527ff43433a4e7664af
praveenkumarpgiindia <praveenkumarpgiindia@gmail.com>
2021-09-10 20:28:34 +0530

    finalised editing of topol file

--- topolmodification/edittopol.sh
+++ topolmodification/edittopol.sh
@@ -5,7 +5,9 @@ x=$1
 y=$2
 echo -e "The topol file selected is $x"
 echo "#include  \"$y\" will be added to the topol file at position number 22"
-sed -i "22 i\#include \"$y\"" $x 
+echo "Also LIG     1 is added in the last line of the topol file"
+sed -i "22 i\#include \"$y\"" $x
+echo "LIG     1" >> $x  
 echo "-----topol.top is ready-----"
 }
 echo "enter topol.top"
