91ce7d0fac3cab27ef7a1eda84ecfbed552d1dff
Praveen Kumar M <praveenkumarpgiindia@gmail.com>
2021-12-25 12:53:59 +0530

    edited  and working for adding topol.top and ligand.itp

--- topolmodification/edittopol.sh
+++ topolmodification/edittopol.sh
@@ -7,7 +7,8 @@ echo -e "The topol file selected is $x"
 echo "; Include ligand topology"
 echo "#include  \"$y\" will be added to the topol file at position number 22"
 echo "Also LIG     1 is added in the last line of the topol file"
-sed -i "22 i\#include \"$y\"" $x
+sed -i "22 i\; Include ligand topology" $x
+sed -i "23 i\#include \"$y\"" $x
 echo "LIG     1" >> $x  
 echo "-----topol.top is ready-----"
 }
